7f67da0e89d2f7ed334fa2218c0cb9050c2d6660
AuxiliumCDNG <soenke.klock@outlook.de>
2020-09-23 15:47:58 +0200

    single charts update with button

--- public/js/display.js
+++ public/js/display.js
@@ -1,3 +1,11 @@
+function updateCharts() {
+    drawCompareChart();
+
+    drawHumidityChart();
+    drawTempChart();
+    drawWeightChart();
+}
+
 function stundenplan() {
     document.getElementById("data-site").style.display = "none";
     document.getElementById("charts").style.display = "none";
@@ -15,9 +23,28 @@ function charts() {
     document.getElementById("data-site").style.display = "none";
     document.getElementById("tgg-frame").style.display = "none";
 
-    drawCompareChart()
-
-    drawHumidityChart();
-    drawTempChart();
-    drawWeightChart();
+    updateCharts();
 }
+
+var timeoutID;
+
+function startTimer() {
+    timeoutID = window.setTimeout(doInactive, 60000);
+}
+function setupTimers () {
+    document.addEventListener("mousemove", resetTimer, false);
+    document.addEventListener("mousedown", resetTimer, false);
+    document.addEventListener("keypress", resetTimer, false);
+    document.addEventListener("touchmove", resetTimer, false);
+     
+    startTimer();
+}
+function doInactive() {
+    home();
+}
+function resetTimer() { 
+    window.clearTimeout(timeoutID)
+    startTimer();
+}
+
+
