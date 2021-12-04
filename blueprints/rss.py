89fa6a3d2b64de4ae5163997321348e1d5348a1b
Fabian Reinders <fabian@fabiancdng.com>
2021-12-04 15:10:01 +0100

    Feeds: Add "Feed Formats" (XML, JSON, HTML)
    Add ability to retrieve a feed in different formats. There's a classic
    RSS (XML) option, a JSON option and an HTML option utilizing the
    Feed Template.

--- blueprints/rss.py
+++ blueprints/rss.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, Response, request, jsonify
+from flask.templating import render_template
 import notifications
 
 Notifications = notifications.Feed()
@@ -7,7 +8,21 @@ rss = Blueprint("rss", __name__)
 
 @rss.route("/<feed>/", methods=["GET"])
 def show_feed(feed):
-    return Notifications.get_feed(feed)
+    # Get the HTTP request's GET params
+    args = dict(request.args)
+
+    # Return pretty, HTML-based version of the feed if ?pretty is passed
+    if "pretty" in args.keys():
+        feed_data = Notifications.get_feed(feed, rss_format=False)
+        return render_template("rss.html", feed_name=feed, records=feed_data)
+    # Return feed as valid JSON if ?json is passed
+    elif "json" in args.keys():
+        feed_data = jsonify(Notifications.get_feed(feed, rss_format=False))
+        return feed_data
+    # Return feed as valid XML (for instance for RSS readers)
+    else:
+        feed_data = Notifications.get_feed(feed, rss_format=True)
+        return Response(feed_data, mimetype="text/xml")
 
 @rss.route("/<feed>/<feed_id>/", methods=["GET"])
 def show_article(feed, feed_id):
