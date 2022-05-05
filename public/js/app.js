6f7cfd9b21f69b85e8f169b30747bfa649f33710
Fabian Reinders <fabian@fabiancdng.com>
2022-05-05 18:51:48 +0200

    Front End: Allow 3-Digit Weight Delta

--- public/js/app.js
+++ public/js/app.js
@@ -212,17 +212,17 @@ async function updateCurrentData(data) {
  */
 function getWeightDeltaString(data) {
     // Get weight from most recent record
-    var weightCurrent = data[Object.keys(data).length - 1].weight;
+    let weightCurrent = data[Object.keys(data).length - 1].weight;
     // Get weight from the record in the middle of the array (measured approximately 24 hours ago)
     // This is done in case there is no record from *exactly* 24 hours ago
-    var weightStartIndex = Math.floor(((Object.keys(data).length - 1) / 2))
-    var weightStart = data[weightStartIndex].weight;
+    let weightStartIndex = Math.floor(((Object.keys(data).length - 1) / 2))
+    let weightStart = data[weightStartIndex].weight;
     // Calculate the delta of the current and start weight
-    var weightDelta = weightCurrent - weightStart;
+    let weightDelta = Number(weightCurrent) - Number(weightStart);
     // Limit float to 2 decimal places
-    weightDelta = Number(weightDelta.toFixed(2));
+    weightDelta = weightDelta.toFixed(3);
     // Format HTML string with green color for weight growth and red color for weight decline
-    var weightDeltaString = weightDelta >= 0 ? `<p style='color: #8aff6b;'>+${weightDelta} kg</p>` : `<p style='color: #fe7373;'>${weightDelta} kg</p>`;
+    let weightDeltaString = weightDelta >= 0 ? `<p style='color: #8aff6b;'>+${weightDelta} kg</p>` : `<p style='color: #fe7373;'>${weightDelta} kg</p>`;
     return weightDeltaString;
 }
 
